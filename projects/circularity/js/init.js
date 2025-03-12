3e0807fb1cbe5a8724db36d0c9c57b3b1e862ffa
Oyasumimono <Oyasumimono@gmail.com>
2025-03-12 18:12:36 +0000

    actual fix to wall sides circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -78,18 +78,17 @@ var init = function (window) {
         game.checkCirclePosition = function (circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if (circle.x >= canvas.width) {
-                circle.x = 0
+            if (circle.x > canvas.width) {
+                circle.x = 10
             }
-
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if (circle.x <= 0) {
-                circle.x = canvas.width
+            if (circle.x < 0) {
+                circle.x = canvas.width //left side//
             }
-            if (circle.y <= 0) {
+            if (circle.y < 0) { 
                 circle.y = canvas.height
             }
-            if (circle.y >= canvas.height) {
+            if (circle.y > canvas.height) { 
                 circle.y = 0
             }
 
